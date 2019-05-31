1e62010cfc30dccfdf8173c3eea25c4190585985
Daniel Staff <daniel.r.staff@gmail.com>
2019-05-31 16:07:28 +0100

    Update setup.py
    use correct project url

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='ml_flask_tutorial',
     version='0.0.1',
-    url='https://github.com/mypackage.git',
+    url='https://github.com/Dan-Staff/ml_flask_tutorial.git',
     author='Daniel Staff',
     author_email='daniel.r.staff@gmail.com',
     description='An example of using flask to wrap a predictive model',
